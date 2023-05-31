ff89a1f87ea86d99b920bc72589659fbbc2ae278
dwgdbima <dewagedebima@gmail.com>
2023-05-31 17:15:06 +0800

    initial commit

---
+++ config/ipaymu.php
@@ -0,0 +1,10 @@
+<?php
+  
+return [
+    'apiKey' => 'your-apikey', //Your Api Key Ipaymu
+    'va' => 'your-va', // Your VA Ipaymu
+    'production' => false,
+    'ureturn' => 'https://ipaymu.com/return',
+    'ucancel' => 'https://ipaymu.com/cancel',
+    'unotify' => 'https://ipaymu.com/notify'
+];
